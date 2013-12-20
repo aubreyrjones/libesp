0ae16bb966a1347b4f1bde43fb9e875fb9204b22
Aubrey R. Jones <aubrey@greyhelix.com>
2013-12-19 20:37:33 -0500

    Trying to improve the structure of the sqlite database.

--- unit.sh
+++ unit.sh
@@ -2,5 +2,5 @@ rm -f *.sqlite
 rm -f *.esp
 ./tests/test2
 ./esp_uplift test2.esp
-./sqlite session_stream.sqlite "select * from session_events;"
-./sqlite session_stream.sqlite "select * from session_strings;"
+./sqlite session_stream.sqlite "select * from events;"
+./sqlite session_stream.sqlite "select * from strings;"
